cd939cad8a3725305ca787316271dbc93e6b6565
kelvin <kelvin@kelvins-MacBook-Pro.local>
2026-09-26 12:20:07 +0000

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,11 @@ export const metadata: Metadata = {
   description: defaultDescription,
   icons: {
     icon: [
-      { url: "/favicon.ico" },
+      { url: "/favicon.ico", sizes: "48x48" },
+      { url: "/icon-192.png", type: "image/png", sizes: "192x192" },
       { url: "/favicon.png", type: "image/png", sizes: "512x512" },
     ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
   },
   alternates: {
     canonical: "/",
